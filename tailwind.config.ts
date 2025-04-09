a0263e11977fc64cd261df2bcc1c443c8fd646e8
trustedprince01 <chibuzorprince68@gmail.com>
2025-04-09 07:09:32 -0700

    Update index.html with Open Graph meta tags and adjust image path; enhance tailwind.config.ts with additional font sizes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -69,6 +69,19 @@ export default {
 				md: 'calc(var(--radius) - 2px)',
 				sm: 'calc(var(--radius) - 4px)'
 			},
+			fontSize: {
+				'xs': ['0.75rem', '1rem'], // 12px
+				'sm': ['0.875rem', '1.25rem'], // 14px
+				'base': ['1rem', '1.5rem'], // 16px
+				'lg': ['1.125rem', '1.75rem'], // 18px
+				'xl': ['1.25rem', '1.75rem'], // 20px
+				'2xl': ['1.5rem', '2rem'], // 24px
+				'3xl': ['1.875rem', '2.25rem'], // 30px
+				'4xl': ['2.25rem', '2.5rem'], // 36px
+				'5xl': ['3rem', '1'], // 48px
+				'6xl': ['3.75rem', '1'], // 60px
+				'7xl': ['4.5rem', '1'], // 72px
+			},
 			keyframes: {
 				'accordion-down': {
 					from: {
